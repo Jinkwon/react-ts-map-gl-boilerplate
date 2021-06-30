2b12c193ebd2c4634c58e1841061c8ac42a55149
jinkwon.lee <jinkwon.lee@42dot.ai>
2021-06-30 10:13:41 +0900

    task: update env

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const smp = new SpeedMeasurePlugin();
 
-module.exports = merge(common, {
+module.exports = smp.wrap(merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
   },
-});
+}));
